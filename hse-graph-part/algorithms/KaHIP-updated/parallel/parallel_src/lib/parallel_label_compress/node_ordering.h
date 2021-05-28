8450bf9dec32ae51d46b21e22d089d4c7a096789
Mark <krnejchik@mail.ru>
2021-05-28 18:41:53 +0300

    delete build folder

--- hse-graph-part/algorithms/KaHIP-updated/parallel/parallel_src/lib/parallel_label_compress/node_ordering.h
+++ hse-graph-part/algorithms/KaHIP-updated/parallel/parallel_src/lib/parallel_label_compress/node_ordering.h
@@ -45,25 +45,45 @@ public:
                 random_functions::permutate_vector_fast(ordered_nodes, false);
         }
 
-        void order_nodes_degree(const PPartitionConfig & config, parallel_graph_access & G, std::vector< NodeID > & ordered_nodes) { 
-                std::sort( ordered_nodes.begin(), ordered_nodes.end(), 
-                           [&]( const NodeID & lhs, const NodeID & rhs) -> bool {
-                                return (G.getNodeDegree(lhs) < G.getNodeDegree(rhs));
-                           });
+        void order_nodes_degree(const PPartitionConfig & config, parallel_graph_access & G, std::vector< NodeID > & ordered_nodes) {
+                static std::vector< size_t > count;
+                static std::vector< std::pair< EdgeID, NodeID > > ordered_nodes_with_degrees;
+                ordered_nodes_with_degrees.clear();
+                for (NodeID node_id : ordered_nodes) {
+                        ordered_nodes_with_degrees.emplace_back(G.getNodeDegree(node_id), node_id);
+                }
+                auto values_range = std::minmax_element(
+                        ordered_nodes_with_degrees.begin(), 
+                        ordered_nodes_with_degrees.end()
+                );
+                if (values_range.second->first - values_range.first->first + 1 <= ordered_nodes.size() * 2) {
+                        NodeID OFFSET = values_range.first->first;
+                        size_t COUNT = values_range.second->first - values_range.first->first + 1;
+                        count.assign(COUNT, 0);
+                        for (const auto& it: ordered_nodes_with_degrees) {
+                                ++count[it.first - OFFSET];
+                        }
+                        for (int i = 1; i < COUNT; i++) {
+                                count[i] += count[i - 1];
+                        }
+                        for (const auto& it: ordered_nodes_with_degrees) {
+                                size_t pos = --count[it.first - OFFSET];
+                                ordered_nodes[pos] = it.second;
+                        }
+                } else {
+                        std::sort(ordered_nodes_with_degrees.begin(), ordered_nodes_with_degrees.end());
+                        for (int i = 0; i < ordered_nodes.size(); ++i) {
+                                ordered_nodes[i] = ordered_nodes_with_degrees[i].second;
+                        }
+                }          
         }
 
         void order_leastghostnodes_nodes_degree(const PPartitionConfig & config, parallel_graph_access & G, std::vector< NodeID > & ordered_nodes) { 
-                std::sort( ordered_nodes.begin(), ordered_nodes.end(), 
-                           [&]( const NodeID & lhs, const NodeID & rhs) -> bool {
-                                return (G.getNodeDegree(lhs) < G.getNodeDegree(rhs));
-                           });
+                order_nodes_degree(config, G, ordered_nodes);
         }
 
 	void order_nodes_degree_leastghostnodes(const PPartitionConfig & config, parallel_graph_access & G, std::vector< NodeID > & ordered_nodes) { 
-                std::sort( ordered_nodes.begin(), ordered_nodes.end(), 
-                           [&]( const NodeID & lhs, const NodeID & rhs) -> bool {
-				return (G.getNodeDegree(lhs) < G.getNodeDegree(rhs));
-                           });
+                order_nodes_degree(config, G, ordered_nodes);
         }
 };
 
